d88f1e582d01a99e648bd374ef393f2f976f46e2
root <brianlawney@gmail.com>
2018-09-20 00:06:34 +0000

    Some cleanup

--- transfer_app/urls.py
+++ transfer_app/urls.py
@@ -51,17 +51,16 @@ urlpatterns.extend([
     #re_path(r'^oauth-dev/drive/$', views.test_drive, name='drive_oauth-test'),
     #re_path(r'^oauth-dev/drive-callback/$', DriveDownloader.finish_authentication_and_start_download, name='drive_token_callback_test'),
 
-    re_path(r'^oauth-dev/test/$', live_oauth2_tests.live_test),
-    re_path(r'^oauth-dev/test/dropbox/$', live_oauth2_tests.dropbox_code_exchange_test, name='live_oauth_test_dropbox'),
-    re_path(r'^oauth-dev/test/dropbox-callback/$', live_oauth2_tests.dropbox_token_exchange_test),
-    re_path(r'^oauth-dev/test/drive/$', live_oauth2_tests.drive_code_exchange_test, name='live_oauth_test_drive'),
-    re_path(r'^oauth-dev/test/drive-callback/$', live_oauth2_tests.drive_token_exchange_test),
+    re_path(r'^oauth-dev/test/$', live_tests.live_test),
+    re_path(r'^oauth-dev/test/dropbox/$', live_tests.dropbox_code_exchange_test, name='live_oauth_test_dropbox'),
+    re_path(r'^oauth-dev/test/dropbox-callback/$', live_tests.dropbox_token_exchange_test),
+    re_path(r'^oauth-dev/test/drive/$', live_tests.drive_code_exchange_test, name='live_oauth_test_drive'),
+    re_path(r'^oauth-dev/test/drive-callback/$', live_tests.drive_token_exchange_test),
 
-    re_path(r'^oauth-dev/transfer-test/$', live_transfer_tests.live_transfer_test),
-    re_path(r'^oauth-dev/transfer-test/dropbox/$', live_transfer_tests.dropbox_code_exchange_test, name='live_transfer_test_dropbox'),
-    re_path(r'^oauth-dev/transfer-test/dropbox-callback/$', live_transfer_tests.dropbox_token_exchange_test),
-    re_path(r'^oauth-dev/transfer-test/drive/$', live_transfer_tests.drive_code_exchange_test, name='live_transfer_test_drive'),
-    re_path(r'^oauth-dev/transfer-test/drive-callback/$', live_transfer_tests.drive_token_exchange_test)
+    re_path(r'^oauth-dev/transfer-test/dropbox/$', live_tests.dropbox_code_exchange_transfer_test, name='live_transfer_test_dropbox'),
+    re_path(r'^oauth-dev/transfer-test/dropbox-callback/$', live_tests.dropbox_token_exchange_transfer_test),
+    re_path(r'^oauth-dev/transfer-test/drive/$', live_tests.drive_code_exchange_transfer_test, name='live_transfer_test_drive'),
+    re_path(r'^oauth-dev/transfer-test/drive-callback/$', live_tests.drive_token_exchange_transfer_test)
 ])
 
 
